d1a152a800a6010863be803d62d2528d0995af03
doanhha <dhavan4444@gmail.com>
2026-04-14 08:20:41 +0700

    reactjs

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import './assets/css/styles.css';
+import Header from './layout/Header';
+
 
 const App = () => {
   return (
     <>
-      
+      <Header/>
     </>
   )
 }
